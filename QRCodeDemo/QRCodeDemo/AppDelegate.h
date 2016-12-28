09636590348307c7c3ff65c322d1ec76538bb325
Kity_Pei <kity_pei@163.com>
2016-12-28 17:33:50 +0800

    新建空项目

---
+++ QRCodeDemo/QRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeDemo
+//
+//  Created by Kity_Pei on 2016/12/28.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeDemo/QRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeDemo
+//
+//  Created by Kity_Pei on 2016/12/28.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
